9070ed2befef5e223e057cd4e813f442a5311cb8
Bobby Norton <bobby@testedminds.com>
2019-06-05 12:48:19 -0500

    Fix data path

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -17,7 +17,7 @@ import pandas as pd
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SALARY_DATA = pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]})
+SALARY_DATA = pd.read_csv(BASE_DIR + '/data/chicago-salaries-processed.csv')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
